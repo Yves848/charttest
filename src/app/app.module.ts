c01a6c94b5364aa29cf40aa5ed6b95c5ad7b7d07
yves848 <yves.godart@gmail.com>
2022-07-13 11:39:58 +0200

    Start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { CardModule } from 'primeng/card';
+import { ChartModule } from 'primeng/chart';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  data: any;
+  options: any;
+  width: number;
+  height: number;
+  constructor() {
+    this.data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          borderColor: '#42A5F5',
+          tension: .4
+        }
+      ]
+    }
+
+    this.options = {
+      title: {
+        display: true,
+        text: 'My Title',
+        fontSize: 16
+      },
+      legend: {
+        position: 'bottom'
+      }
+    };
+
+    //this.width = document.body.offsetWidth;
+    this.width = window.innerWidth / 2;
+    this.height = window.innerHeight;
+    console.log(this.width, this.height);
+  }
+
+
+  ngOnInit(): void {
+  }
+
+}
